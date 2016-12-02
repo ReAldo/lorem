9a331e79eb3d598c94487e2b64c163716f76f4ec
mijara <marjara35@gmail.com>
2016-12-02 01:20:17 -0300

    Add API Rest

--- loremaldo/phrases/urls.py
+++ loremaldo/phrases/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
+import views
 
 urlpatterns = [
-
+    url(r'^$', views.PhraseList.as_view(), name='phrase_list'),
+    url(r'^lorem/$', views.RandomLorem.as_view(), name='lorem'),
+    url(r'^lorem/(?P<n>\d+)/$', views.RandomLorem.as_view(), name='lorem'),
 ]
--- loremaldo/phrases/views.py
+++ loremaldo/phrases/views.py
@@ -1,2 +1,21 @@
-from django.views import generic
+from rest_framework import generics
+from serializers import *
+from rest_framework import views
+import random
 
+
+class RandomLorem(views.APIView):
+    def get(self, request, n=5):
+        rng = xrange(0, Phrase.objects.count())
+        choices = random.sample(rng, int(n))
+
+        phrases = []
+        for i in choices:
+            phrases.append(Phrase.objects.all()[i])
+
+        return views.Response(PhraseSerializer(phrases, many=True).data)
+
+
+class PhraseList(generics.ListAPIView):
+    serializer_class = PhraseSerializer
+    queryset = Phrase.objects.all()
